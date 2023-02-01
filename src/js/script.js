0b368fa69c63209ef6cd86ff9f3bd1b3ed3587fb
m4uriciosn <mauriciososmaiarnascimento@gmail.com>
2023-02-01 11:54:52 -0300

    adiciona js

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+function chamarPercy() {
+    window.location = 'https://acampamentomeio-sangue.fandom.com/pt-br/wiki/O_Ladrão_de_Raios'
+}
+
+function chamarPersonas() {
+    window.location = 'https://ohobrasil.wordpress.com/personagens/'
+}
+
+function chamarAtores() {
+    window.location = 'https://www.adorocinema.com/series/serie-26596/temporada-38548/elenco/'
+}
